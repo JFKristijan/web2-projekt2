80519564cad861d9b6ebb34866e52368335cef60
Fran Kristijan Jelencic <fran@croline.com>
2021-12-12 17:47:31 +0100

    labos progress

--- db/index.js
+++ db/index.js
@@ -1,4 +1,4 @@
-const sqlite3 = require('sqlite3')
+const sqlite3 = require('sqlite3').verbose()
 const db = new sqlite3.Database(':memory:', (err) => {
     if (err) {
         console.log('Could not connect to database', err)
@@ -7,20 +7,61 @@ const db = new sqlite3.Database(':memory:', (err) => {
     }
 });
 
-const tableSql = `CREATE TABLE  IF NOT EXISTS users
+const tableSqli = `CREATE TABLE  IF NOT EXISTS users
                     (id INTEGER PRIMARY KEY AUTOINCREMENT,
                     username text NOT NULL,
-                    password text NOT NULL)`;  
-const usersSql = `INSERT INTO users(username, password) VALUES 
-('Fran','Lozinka'),
-('Ivan','password'),
-('RPPP','b910459hvpdfh-j9g5')`;                   
+                    email text NOT NULL,
+                    credit_card text NOT NULL,
+                    password text NOT NULL);
+                    `;  
+const tableSql = `
+CREATE TABLE  IF NOT EXISTS statuses
+(id INTEGER PRIMARY KEY AUTOINCREMENT,
+username text NOT NULL,
+status text NOT NULL);`
+
+const usersSql = `INSERT INTO users(username,email,credit_card, password) VALUES 
+('Tll6','vrucidr@gmail.cm','123456789','Lozinka'),
+('qwert','mlakidr@gmail.cm','987654321','password'),
+('azer','hladnidr@gmail.cm','123789456','b910459hvpdfh-j9g5'),
+('Kijewski', 'Kijewski@gmail.com', '757554437701', 'JYAQZVOUPBXU'),
+('Mark', 'Mark Pollock@gmail.com', '086500289279', 'IXYRQRVEHNZT'),
+('WiffleBird', 'WiffleBird@gmail.com', '171923340646', 'XMTAVRWGDQCM'),
+('Matthews', 'secretchimp69@gmail.com', '298176315638', 'GSTUWOWWEAHL'),
+('LadyeCatte', 'mizzcatt@gmail.com', '283757825824', 'GKRVHHCRTVKN'),
+('RPG Mom', 'RPG Mom@gmail.com', '030266074287', 'IEVPLUHKHFUL'),
+('John', 'JohnE@gmail.com', '716554316184', 'UUULUTYHLRYT'),
+('Vesko', 'VeskoGavrilov@gmail.com', '971011873566', 'DZTYPRKLITGX'),
+('Wolf22', 'WingedWolf22@gmail.com', '254989884237', 'MAITIHOJKWAR'),
+('Bruce Gray', 'BruceGray@gmail.com', '759975700612', 'GOVLPZWBEPHN'),
+('Brian S', 'BrianSPratt.@gmail.com', '545783874695', 'NSJZBXTGNKBS'),
+('Rene', 'ReneSHoepelmanJr@gmail.com', '188963295422', 'IAQOTOHKXLVS'),
+('Beyond', 'ReneBeyond@gmail.com', '406849281215', 'IASYXTOBJDER');`;            
+
+const statusSql = `INSERT INTO statuses(username,status) VALUES 
+('Fran','Ovo je moj prvi demo status :\)')
+` 
+
 
 db.run(tableSql,(err,result)=>{
     if (err) {
         return console.error(err.message);
     }
-    console.log("Successful creation of the 'inventory' table");
+    console.log("Successful creation of the 'statuses' table");
+    db.run(statusSql,(err,result)=>{
+        if (err) {
+            return console.error(err.message);
+        }
+        console.log("Seeded statuses table");
+    
+    })
+});
+
+db.run(tableSqli,(err,result)=>{
+    if (err) {
+        return console.error(err.message);
+    }
+    console.log("Successful creation of the 'users' table");
     db.run(usersSql,(err,result)=>{
         if (err) {
             return console.error(err.message);
@@ -28,13 +69,13 @@ db.run(tableSql,(err,result)=>{
         console.log("Seeded users table");
     
     })
-    db.all(`select * from users`,[],(err,result)=>{
-        if (err) {
-            return console.error(err.message);
-        }
-        console.log('Done')
-        result.forEach((row)=>{console.log(row)})
-    })
+    // db.all(`select * from users`,[],(err,result)=>{
+    //     if (err) {
+    //         return console.error(err.message);
+    //     }
+    //     console.log('Done')
+    //     result.forEach((row)=>{console.log(row)})
+    // })
 })
 
 
--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-const e = require('express');
 const express = require('express');
 const dotenv = require('dotenv').config();
-
+const validator = require('validator')
 const PORT = process.env.PORT || 8080
 const app = express();
+const session = require('express-session')
 app.use(express.urlencoded())
 const db = require('./db/index')
+
 app.use(express.static('public'));
 app.set('view engine', 'ejs')
+app.use(session({
+  secret: 'play him off, keyboard cat',
+  resave: true,
+  saveUninitialized: true,
+}))
 
 var securityLevel = 0;
 
@@ -25,37 +31,35 @@ app.get('/sql', function (req, res) {
 });
 
 app.post('/sql', function (req, res) {
-
-  console.log(req.body);
   let userSql;
   let params;
   if (securityLevel == 0) {
-    userSql = "select id,password from users where lower(username) like '" + req.body.username + "';"
-    params=[]
-  }else{
-    userSql = "select id,password from users where lower(username) like '?';"
-    params=[req.body.username]
+    userSql = `select username,email from users where lower(username) like '${req.body.username}';`
+    params = []
+  } else {
+    userSql = `select username,email from users where lower(username) = ?;`
+    params = [req.body.username.toLowerCase()]
+
   }
 
-    db.all(userSql, params, (err, result) => {
-      if (err) {
-        console.log('Error' + err.message);
-      }
-      console.log('Done')
-      if (result) {
-        result.forEach((row) => {
-          console.log(row)
-        })
-        users = result
-      } else {
-        users = []
-      }
-      res.render('pages/sql', {
-        securityLevel: securityLevel,
-        active: 'sql',
-        users: users
-      });
+  db.all(userSql, params, (err, result) => {
+    if (err) {
+      console.log('Error' + err.message);
+    }
+    if (result) {
+      result.forEach((row) => {
+        console.log(row)
+      })
+      users = result
+    } else {
+      users = []
+    }
+    res.render('pages/sql', {
+      securityLevel: securityLevel,
+      active: 'sql',
+      users: users
     });
+  });
 
 });
 
@@ -66,11 +70,118 @@ app.get('/xss', function (req, res) {
   });
 });
 
-app.get('/csrf', function (req, res) {
-  res.render('pages/csrf', {
+app.post('/xss', function (req, res) {
+  let name = req.body.name
+  if (securityLevel == 1) {
+    name = validator.escape(name)
+  }
+  res.render('pages/xss', {
     securityLevel: securityLevel,
-    active: 'csrf'
+    active: 'xss',
+    name: name
   });
+})
+
+app.get('/csrf', function (req, res) {
+  let status;
+  let user;
+  if (req.session && req.session.user) {
+    user = req.session.user
+  }
+  db.all('select username, status from statuses', [], (err, result) => {
+    if (err) {
+      console.log('Error' + err.message);
+    }
+    if (result) {
+      result.forEach((row) => {
+        console.log(row)
+      })
+      status = result
+    } else {
+      status = []
+    }
+
+    res.render('pages/csrf', {
+      securityLevel: securityLevel,
+      active: 'csrf',
+      status: status,
+      user: user
+    });
+  });
+
+});
+
+app.post('/csrf', function (req, res) {
+  let user;
+  if (req.body.username) {
+    req.session.user = req.body.username;
+    user = req.body.username;
+  }
+  db.all('select username, status from statuses', [], (err, result) => {
+    if (err) {
+      console.log('Error' + err.message);
+    }
+    if (result) {
+      result.forEach((row) => {
+        console.log(row)
+      })
+      status = result
+    } else {
+      status = []
+    }
+
+    res.render('pages/csrf', {
+      securityLevel: securityLevel,
+      active: 'csrf',
+      status: status,
+      user: user
+    });
+  });
+});
+
+app.get('/status', function (req, res) {
+  if (securityLevel == 1) {
+    res.redirect('/csrf');
+  } else {
+    let user;
+    if (req.session.user) {
+      db.run('insert into statuses(username,status) values (?,?)',
+        [req.session.user, req.query.status],
+        (err, result) => {
+          if (err) {
+            console.log('Error' + err.message);
+          }
+          res.redirect('/csrf')
+        });
+
+    }else{
+      res.status(404).end()
+    }
+  }
+});
+
+app.post('/status', function (req, res) {
+  if (securityLevel == 0) {
+    res.redirect('/csrf');
+  }
+  let user;
+  if (req.session.user) {
+    db.run('insert into statuses(username,status) values (?,?)',
+      [req.session.user, req.query.status],
+      (err, result) => {
+        if (err) {
+          console.log('Error' + err.message);
+        }
+        res.redirect('/csrf')
+      });
+  }else{
+    res.status(404).end()
+  }
+});
+
+app.post('/logout', function (req, res) {
+  req.session.destroy();
+  res.status(200).end();
 });
 
 app.post('/seclevel', function (req, res) {
@@ -79,5 +190,12 @@ app.post('/seclevel', function (req, res) {
     securityLevel: securityLevel
   });
 });
+
+app.get("/pictures", (req, res) => {
+  res.render('pages/pictures', {
+    securityLevel: securityLevel,
+    active: 'pics'
+  })
+})
 
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
